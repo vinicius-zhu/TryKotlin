c7c27ba28bf8d22fd8e39fe331957f0a1f4e8b28
northonh <nobile.pedro@gmail.com>
2019-02-15 21:24:26 -0200

    Variável mutável com VAR e String Templates

--- app/src/main/java/br/edu/ifsp/scl/sdm/main.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/main.kt
@@ -2,7 +2,20 @@ package br.edu.ifsp.scl.sdm
 
 // args: Array<String> deixou de ser obrigatório na versão 1.3
 fun main() {
-    // Escreve na saída padrão e muda de linha
-    println("Hello, World!")
+    // Declaração de variável MUTÁVEL
+    var frase: String = "Hello, World!"
+
+    // Imprime a variável na tela
+    println(frase)
+
+    // Imprimindo String Template
+    println("$frase")
+    println("${frase}")
+
+    // Com chamada de função
+    println("Quantidade de caracteres: ${frase.count()}")
+
+    // Com propriedade (get)
+    println("Quantidade de caracteres: ${frase.length}")
 }
 
