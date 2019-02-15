cf071ecfbf2b655f192f1945e5ca8dc4ee9f129a
northonh <nobile.pedro@gmail.com>
2019-02-15 21:10:36 -0200

    Primeiro exemplo: Hello, World

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/main.kt
@@ -0,0 +1,8 @@
+package br.edu.ifsp.scl.sdm
+
+// args: Array<String> deixou de ser obrigatório na versão 1.3
+fun main() {
+    // Escreve na saída padrão e muda de linha
+    println("Hello, World!")
+}
+
