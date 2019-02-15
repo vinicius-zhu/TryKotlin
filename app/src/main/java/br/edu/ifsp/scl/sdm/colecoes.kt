8811051d32d2f37ade0c5fd9c5a18a5867e954ff
northonh <nobile.pedro@gmail.com>
2019-02-15 21:56:42 -0200

    List usando FOR com IntRange

--- app/src/main/java/br/edu/ifsp/scl/sdm/colecoes.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/colecoes.kt
@@ -10,4 +10,12 @@ fun main() {
     for (integrante in familia){
         println(integrante)
     }
+
+    val listaInteiros: List<Int> = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    // Percorrer usando FOR usando IntRange
+    for (i in 0..9) {
+        println(listaInteiros[i])
+    }
+
+    // listaInteiros.add(11)
 }
